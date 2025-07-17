ce1ecdaed6834f7c941ef6edb644417d20a9a899
Rafael Soares <r.soares2015@outlook.com>
2025-07-17 20:44:05 -0300

    initial commit

---
+++ src/main/java/com/rafaelsisoares/car_dealership/ProjectCarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsisoares.car_dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCarDealershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelsisoares/car_dealership/ProjectCarDealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsisoares.car_dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCarDealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
