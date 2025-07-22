08428b9263c2728baa1e4f919783718337437963
Rafael Soares <r.soares2015@outlook.com>
2025-07-22 20:51:09 -0300

    feat: cria a entidade de Person

---
+++ src/main/java/com/rafaelsisoares/car_dealership/entities/Person.java
@@ -0,0 +1,73 @@
+package com.rafaelsisoares.car_dealership.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String role;
+
+    public Person(String firstName, String role, String password, String username, String lastName) {
+        this.firstName = firstName;
+        this.role = role;
+        this.password = password;
+        this.username = username;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
