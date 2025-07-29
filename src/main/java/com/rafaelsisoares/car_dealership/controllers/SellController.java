fb647b92b445ec0bc2aa2cf59461c7e43e6fa960
Rafael Soares <r.soares2015@outlook.com>
2025-07-29 20:21:59 -0300

    feat: cria o controller e os primeiros endpoints de sell

---
+++ src/main/java/com/rafaelsisoares/car_dealership/controllers/SellController.java
@@ -0,0 +1,40 @@
+package com.rafaelsisoares.car_dealership.controllers;
+
+import com.rafaelsisoares.car_dealership.controllers.dto.SellDto;
+import com.rafaelsisoares.car_dealership.services.SellService;
+import com.rafaelsisoares.car_dealership.services.exceptions.CarNotFoundException;
+import com.rafaelsisoares.car_dealership.services.exceptions.PersonNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sells")
+public class SellController {
+    private final SellService sellService;
+
+    @Autowired
+    public SellController(SellService sellService) {
+        this.sellService = sellService;
+    }
+
+    @PostMapping("/car/{carId}/seller/{sellerId}/customer/{customerId}")
+    public ResponseEntity<SellDto> createSell(@PathVariable Long carId,
+                                              @PathVariable Long sellerId,
+                                              @PathVariable Long customerId)
+            throws CarNotFoundException, PersonNotFoundException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                SellDto.fromEntity(sellService.createSell(carId, sellerId, customerId))
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SellDto>> findAllSells() {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                sellService.findAllSells().stream().map(SellDto::fromEntity).toList()
+        );
+    }
+}
