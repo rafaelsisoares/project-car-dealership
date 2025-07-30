99fd36e1dc22d82378c676837703a5606d820b1f
Rafael Soares <r.soares2015@outlook.com>
2025-07-30 19:48:45 -0300

    chore: adiciona a nova excecao

--- src/main/java/com/rafaelsisoares/car_dealership/controllers/SellController.java
+++ src/main/java/com/rafaelsisoares/car_dealership/controllers/SellController.java
@@ -3,7 +3,9 @@ package com.rafaelsisoares.car_dealership.controllers;
 import com.rafaelsisoares.car_dealership.controllers.dto.SellDto;
 import com.rafaelsisoares.car_dealership.services.SellService;
 import com.rafaelsisoares.car_dealership.services.exceptions.CarNotFoundException;
+import com.rafaelsisoares.car_dealership.services.exceptions.CarUnavailableException;
 import com.rafaelsisoares.car_dealership.services.exceptions.PersonNotFoundException;
+import com.rafaelsisoares.car_dealership.services.exceptions.SamePersonException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +27,7 @@ public class SellController {
     public ResponseEntity<SellDto> createSell(@PathVariable Long carId,
                                               @PathVariable Long sellerId,
                                               @PathVariable Long customerId)
-            throws CarNotFoundException, PersonNotFoundException {
+            throws CarNotFoundException, PersonNotFoundException, SamePersonException, CarUnavailableException {
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 SellDto.fromEntity(sellService.createSell(carId, sellerId, customerId))
         );
