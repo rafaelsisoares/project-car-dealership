2678ff0c98deaec4d303af1dc04b0a08a3f03575
Rafael Soares <r.soares2015@outlook.com>
2025-07-30 19:47:55 -0300

    chore: adiciona as novas excecoes

--- src/main/java/com/rafaelsisoares/car_dealership/controllers/advice/GlobalControllerAdvice.java
+++ src/main/java/com/rafaelsisoares/car_dealership/controllers/advice/GlobalControllerAdvice.java
@@ -1,7 +1,9 @@
 package com.rafaelsisoares.car_dealership.controllers.advice;
 
 import com.rafaelsisoares.car_dealership.services.exceptions.CarNotFoundException;
+import com.rafaelsisoares.car_dealership.services.exceptions.CarUnavailableException;
 import com.rafaelsisoares.car_dealership.services.exceptions.PersonNotFoundException;
+import com.rafaelsisoares.car_dealership.services.exceptions.SamePersonException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +20,15 @@ public class GlobalControllerAdvice {
     @ExceptionHandler
     public ResponseEntity<String> handlePersonNotFoundException(PersonNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleSamePersonException(SamePersonException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleCarUnavailableException(CarUnavailableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
