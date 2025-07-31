1c95317f81ac0b4ad56ce49ac871da7747129f40
Rafael Soares <r.soares2015@outlook.com>
2025-07-31 20:15:08 -0300

    chore: cria o service do JWT e os metodos de criacao e validacao de token

---
+++ src/main/java/com/rafaelsisoares/car_dealership/services/TokenService.java
@@ -0,0 +1,27 @@
+package com.rafaelsisoares.car_dealership.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class TokenService {
+    private Algorithm algorithm;
+
+    public TokenService() {
+        this.algorithm = Algorithm.HMAC256("${authentication.jwt.secret}");
+    }
+
+    private Instant generateExpiration() {
+        return Instant.now().plus(2, ChronoUnit.HOURS);
+    }
+
+    public String generateToken(String username) {
+        return JWT.create().withSubject(username).withExpiresAt(generateExpiration()).sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        return JWT.require(algorithm).build().verify(token).getSubject();
+    }
+}
