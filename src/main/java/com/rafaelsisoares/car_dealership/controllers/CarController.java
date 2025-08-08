e784e3b13552d76678490f307000cea56a19c974
Rafael Soares <r.soares2015@outlook.com>
2025-08-07 21:03:22 -0300

    chore: adiciona as regras de segurança nos novos endpoints

--- src/main/java/com/rafaelsisoares/car_dealership/controllers/CarController.java
+++ src/main/java/com/rafaelsisoares/car_dealership/controllers/CarController.java
@@ -47,6 +47,7 @@ public class CarController {
     }
 
     @GetMapping("/brand/{brand}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('SELLER') or hasAuthority('CUSTOMER')")
     public ResponseEntity<List<CarDto>> findCarsByBrand(@PathVariable String brand) {
         return ResponseEntity.status(HttpStatus.OK).body(
                 carService.findCarsByBrand(brand).stream().map(CarDto::fromEntity).toList()
@@ -54,6 +55,7 @@ public class CarController {
     }
 
     @GetMapping("/model/{model}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('SELLER') or hasAuthority('CUSTOMER')")
     public ResponseEntity<CarDto> findCarByModel(@PathVariable String model) throws CarNotFoundException {
         return ResponseEntity.status(HttpStatus.OK).body(CarDto.fromEntity(carService.findCarByModel(model)));
     }
